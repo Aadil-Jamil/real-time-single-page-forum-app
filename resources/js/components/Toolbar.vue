d3bc97d8911d03a052b0b069ef906974c10294d3
Aadil Jamil <aadiljamil619@gmail.com>
2020-04-02 13:57:59 +0500

    Login component and Login

--- resources/js/components/Toolbar.vue
+++ resources/js/components/Toolbar.vue
@@ -3,12 +3,20 @@
 <!--        <v-toolbar-side-icon></v-toolbar-side-icon>-->
         <v-toolbar-title>Bitfumes</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-toolbar-items class="hidden-sm-and-down">
-            <v-btn text>Forum</v-btn>
-            <v-btn text>Ask Question</v-btn>
-            <v-btn text>Category</v-btn>
-            <v-btn text>Login</v-btn>
-        </v-toolbar-items>
+        <div class="hidden-sm-and-down">
+            <router-link to="/forum">
+                <v-btn text>Forum</v-btn>
+            </router-link>
+<!--            <router-link>-->
+                <v-btn text>Ask Question</v-btn>
+<!--            </router-link>-->
+<!--            <router-link>-->
+                <v-btn text>Category</v-btn>
+<!--            </router-link>-->
+            <router-link to="/login">
+                <v-btn text>Login</v-btn>
+            </router-link>
+        </div>
     </v-toolbar>
 </template>
 
